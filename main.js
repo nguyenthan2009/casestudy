14edfaa1e0770bfdaa5dbdff8284978cff61d6e2
unknown <sauvole75@gmail.com>
2021-06-30 08:15:52 +0700

    casestudy

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var btn_anh= document.getElementById('chon')
 var btn_anh1=document.getElementById('chon1')
 var anh= document.getElementById('chonanh')
 var anh1= document.getElementById('chonanh1')
+var danhsach= document.getElementById('ds')
 
 var fly = new Audio();
 var scor = new Audio();
@@ -24,12 +25,9 @@ hinhnenchinh.src="images/nenchinh.png";
 ongtren.src="images/ongtren.png";
 ongduoi.src="images/ongduoi.png";
 
-
-
 var score=0;
 var khoangcachhaiong=140; 
-var khoangcachdenongduoi; // biến này là khoảng cách từ đầu ống trên đến vị trí đầu ống dưới
-// tạo ra 1 object chim với tọa độ x y là 1 nữa canvas
+var khoangcachdenongduoi; // là khoảng cách từ đầu ống trên đến vị trí đầu ống dưới
 var bird={
     x: hinhnenchinh.width/5,
     y: hinhnenchinh.height/2
@@ -37,12 +35,8 @@ var bird={
 var ong=[]; //tạo mảng ống để chứa các ống di chuỷen
 ong[0]={
     x:canvas.width,
-    y:0 // khởi tạo ống đầu tiên nằm bên phải ngoài cùng và y=0;
+    y:0 
 }
-
-
-//tạo function để chạy trò chơi
-
 function run(){
     // load hình ảnh vào
     context.drawImage(hinhnenchinh,0,0);
@@ -51,61 +45,50 @@ function run(){
     for(var i=0;i<ong.length;i++){
         khoangcachdenongduoi=ongtren.height+khoangcachhaiong;
         context.drawImage(ongtren,ong[i].x,ong[i].y);
-        // vẽ ống trên theo tọa độ của ống đó
-        //  ống dưới phụ thuộc ống trên
         context.drawImage(ongduoi,ong[i].x,ong[i].y+khoangcachdenongduoi);
-        // mình lấy vị trí ống trên cộng khoảng cách đến
-        // ống dưới vì tí nữa mình random nó lên xuống
         ong[i].x-=5; //để ống di chuyển
         
 
         // lập trình thêm ống khi ống di chuyển đến giữa
         // nó sẽ tạo thêm 1 ống nữa
-        if(ong[i].x ==canvas.width/2){
-            
+        if(ong[i].x ==canvas.width/2){  
              ong.push({
                 x:canvas.width,
                 y:Math.floor(Math.random()*ongtren.height)-ongtren.height
-            })
+                })
         }
         if(ong[i].x ==0 )ong.splice(0,1);
         // nếu ống đụng lề trái thì xóa nó đi để tránh mảng ống
-        //  bị đầy làm chậm
       
         if(ong[i].x==bird.x){
           score++;
           
           scor.play(); }
-          
-        // game over  
-        if(bird.y+birdimg.height==canvas.height||
-        bird.x+birdimg.width>= ong[i].x && bird.x <= ong[i].x +ongtren.width
-        && (bird.y<=ong[i].y+ongtren.height||
-        bird.y +birdimg.height>= ong[i].y+ khoangcachdenongduoi)    
+         
+          // game over  
+        if(bird.y+birdimg.height==canvas.height||bird.y<0||
+            bird.x+birdimg.width>= ong[i].x && bird.x <= ong[i].x +ongtren.width
+            && (bird.y<=ong[i].y+ongtren.height||
+            bird.y +birdimg.height>= ong[i].y+ khoangcachdenongduoi)    
+            
         ){
             end.style.display= 'block';
             return;
         }                   
     }
-    //  điều kiện đầu tiên là đụng đất
-    // các bạn chú ý là tính tọa độ y cộng với độ cao con chim
-    //  điều kiện thứ hai là so sánh vị trí x con chim
-    // với cái ống 
-    // và cuối cùng là so sánh vị trí y
-
-
 
     scoreshow.innerHTML="Score: " +score;
     // cho bird rơi xuống
     bird.y+=3;
+    danhsach.innerHTML= 'anh yeu em'
     
-    requestAnimationFrame(run);
+    
+ requestAnimationFrame(run);
   
 }
-//thêm function cho nó bay lên khi nhấn
 document.addEventListener("keydown",function(e){
-    if(e.keyCode==38){
-    bird.y-=50;
+    if(e.keyCode==32){
+    bird.y-=60;
     fly.play();   }
 })
 
@@ -117,7 +100,7 @@ start.addEventListener('click',function(){
 end.addEventListener('click',function(){
     end.style.display= 'none';
    var tên = prompt('Mời bạn nhập tên để lưu điểm')
-    luuten.innerHTML =tên + '<br>' + score + "điểm";
+    luuten.innerHTML =tên + '<br>' + score +  "điểm";
     luuten.style.display= 'block';
     restart.style.display= 'block';
 
@@ -126,9 +109,7 @@ restart.addEventListener('click',function(){
     restart.style.display= 'none'
     luuten.style.display= 'none';
     location.reload();
-    run();
 
-    
 })
 btn_anh.addEventListener('click',function(){
     birdimg.src = anh.getAttribute('src')
